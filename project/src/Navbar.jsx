3cebc3696defadf8955cbe733068d9f6f4b3f772
thanthephong21 <thanthephong21082k2@gmail.com>
2024-03-13 22:34:54 +0700

    Ex

--- project/src/Navbar.jsx
+++ project/src/Navbar.jsx
@@ -1,36 +1,34 @@
-import { Link, Outlet } from 'react-router-dom';
-import './Navbar.css';
-import AlbumDropDownButton from './AlbumDropDownButton';
-import Button from 'react-bootstrap/Button';
+import { Link, Outlet } from "react-router-dom";
+import "./Navbar.css";
+import AlbumDropDownButton from "./AlbumDropDownButton";
+import Button from "react-bootstrap/Button";
 
 function Navbar() {
-    return (
-        <div>
-            <div className='navbar-container'>
-
-                <div className='navbar'>
-                    <span className='shop-name'>Rhythm House</span>
-                    <Link to="/">Home</Link>
-                    <AlbumDropDownButton />
-                    <Link to="/events">Events</Link>
-                    <Link to="/membership">Membership</Link>
-                    <Link to="/about">About Us</Link>
-                    <Link to="/contact">Contact Us</Link>
-                </div>
-
-                <div className='navbar-button-container'>
-                    <Button variant="light" className='navbar-button'>Login</Button>
-                    <Button variant="light" className='navbar-button'>Sign Up</Button>
-                </div>
-
-                
-
-
-            </div>
-            <Outlet></Outlet>
+  return (
+    <div>
+      <div className="navbar-container">
+        <div className="navbar">
+          <span className="shop-name">Rhythm House</span>
+          <Link to="/">Phong</Link>
+          <AlbumDropDownButton />
+          <Link to="/events">Events</Link>
+          <Link to="/membership">Membership</Link>
+          <Link to="/about">About Us</Link>
+          <Link to="/contact">Contact Us</Link>
         </div>
 
-    );
+        <div className="navbar-button-container">
+          <Button variant="light" className="navbar-button">
+            Login
+          </Button>
+          <Button variant="light" className="navbar-button">
+            Sign Up
+          </Button>
+        </div>
+      </div>
+      <Outlet></Outlet>
+    </div>
+  );
 }
 
 export default Navbar;
