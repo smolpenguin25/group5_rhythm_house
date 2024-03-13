f44ed8053142be73dbb6ce2ed14b163c50b88817
smolpenguin25 <ngthienphu2502@gmail.com>
2024-03-13 16:53:56 +0700

    change home

--- project/src/Home.jsx
+++ project/src/Home.jsx
@@ -1,7 +1,14 @@
+import './Home.css'
+//import Carousel from 'react-bootstrap/Carousel';
+
 function Home() {
-    return (  
-        <div>
-            ok
+    return (
+        <div className='home-container'>
+            <div className="carousel-container">
+                <div className='carousel-title'>Discounted Products</div>
+                <div className='carousel-content'>
+                </div>
+            </div>
         </div>
     );
 }
