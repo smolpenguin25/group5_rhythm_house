fc5c497dae73815ee30b486f768d1154b0f26e5c
thanthephong21 <thanthephong21082k2@gmail.com>
2024-03-13 22:34:03 +0700

    PhongEventEdit

--- project/src/AlbumDropDownButton.jsx
+++ project/src/AlbumDropDownButton.jsx
@@ -1,17 +1,20 @@
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import { Link, Outlet } from "react-router-dom";
 
 function AlbumDropDownButton() {
-    return (
-        <div>
-            <DropdownButton id="dropdown-basic-button" title="Albums">
-                <Dropdown.Item>Solo</Dropdown.Item>
-                <Dropdown.Item>Band</Dropdown.Item>
-                <Dropdown.Item>Movie</Dropdown.Item>
-                <Dropdown.Item>Magazine</Dropdown.Item>
-            </DropdownButton>
-        </div>
-    );
+  return (
+    <div>
+      <DropdownButton id="dropdown-basic-button" title="Albums">
+        <Dropdown.Item>
+          <Link to="/EventSolo">Solo</Link>
+        </Dropdown.Item>
+        <Dropdown.Item>Band</Dropdown.Item>
+        <Dropdown.Item>Movie</Dropdown.Item>
+        <Dropdown.Item>Magazine</Dropdown.Item>
+      </DropdownButton>
+    </div>
+  );
 }
 
 export default AlbumDropDownButton;
---
+++ project/src/EventSolo.jsx
@@ -0,0 +1,16 @@
+import "./Home.css";
+function EventSolo() {
+  return (
+    <div className="home-container">
+      <div className="carousel-container">
+        <div className="carousel-title">Discounted Products</div>
+        <div className="carousel-content"></div>
+      </div>
+      <div>
+        <h1>Event SOlo</h1>
+      </div>
+    </div>
+  );
+}
+
+export default EventSolo;
