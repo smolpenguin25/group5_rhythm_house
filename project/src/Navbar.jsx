ae4de46d01ce5793d783b9086b4fefdc2ff44be8
nguyenhuuhoang222 <huuhoangnguyen186@gmail.com>
2024-03-15 08:36:56 +0700

    contact

--- project/src/Navbar.jsx
+++ project/src/Navbar.jsx
@@ -2,6 +2,7 @@ import { Link, Outlet } from "react-router-dom";
 import "./Navbar.css";
 import AlbumDropDownButton from "./AlbumDropDownButton";
 import Button from "react-bootstrap/Button";
+import ContactUs from "./ContactUs";
 
 function Navbar() {
   return (
@@ -14,7 +15,7 @@ function Navbar() {
           <Link to="/events">Events</Link>
           <Link to="/membership">Membership</Link>
           <Link to="/about">About Us</Link>
-          <Link to="/contact">Contact Us</Link>
+          <Link to="/ContactUs">Contact Us</Link>
           <span className="search">Enter product name: </span><input type="text" placeholder="Search..." value="" onChange="" />
 
         </div>
