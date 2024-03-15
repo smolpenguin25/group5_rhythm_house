e4d67f8d6b5ab074fc7882595befdd8bc6812a04
smolpenguin25 <ngthienphu2502@gmail.com>
2024-03-15 10:00:07 +0700

    add login

--- project/src/App.js
+++ project/src/App.js
@@ -4,12 +4,7 @@ import Navbar from './Navbar';
 import { Route, Routes } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap-grid.min.css'
 import React, { useEffect } from 'react';
-<<<<<<< HEAD
 import Login from './Login';
-=======
-import ContactUs from  './ContactUs';
-import AboutUsUs from  './AboutUs';
->>>>>>> e633bb33b00f9345f89d6083eb3f1235e4a09ea5
 
 function App() {
   useEffect(() => {
@@ -26,15 +21,9 @@ function App() {
           <Route path='magazine' />
           <Route path='events' />
           <Route path='membership' />
-<<<<<<< HEAD
           <Route path='about' />
           <Route path='contact' />
           <Route path='login' element={<Login/>}/>
-=======
-          <Route path='about' element={<AboutUs/>} />
-          <Route path='contact' element={<ContactUs/>}/>
-          <Route path='login' />
->>>>>>> e633bb33b00f9345f89d6083eb3f1235e4a09ea5
           <Route path='signup' />
         </Route>
       </Routes>
