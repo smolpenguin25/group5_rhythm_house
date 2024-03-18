94f1f2061570f1685ff169dd47c45a5ac19e898d
nguyenhuuhoang222 <huuhoangnguyen186@gmail.com>
2024-03-18 11:08:59 +0700

    Update membership form with new CSS classes and styles.

--- project/src/Membership.jsx
+++ project/src/Membership.jsx
@@ -1,9 +1,10 @@
 
 import MembershipForm from "./MembershipForm";
-
+import './MembershipForm.css';
 function Membership() {
     return (  
         <div className="login-background">
+        <h2>Music Membership Registration</h2>
             <MembershipForm/>
         </div>
     );
