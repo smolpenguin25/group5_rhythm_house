0d4f3b2def27cf96ef5580c42aca61669187229d
nguyenhuuhoang222 <huuhoangnguyen186@gmail.com>
2024-03-25 08:43:28 +0700

    Update ContactUs Form

--- project/src/ContactUs.jsx
+++ project/src/ContactUs.jsx
@@ -1,34 +1,69 @@
 import React from "react";
 import "./ContactUs.css";
 
-export const ContactUs = () => {
+const ContactUs = () => {
   return (
-    <div className="background">
-      <div className="overlap-group-wrapper">
-        <div className="overlap-group">
-          <div className="rectangle" />
-          <div className="div" />
-          <div className="text-wrapper">Contact Us</div>
-          <div className="text-wrapper-2">First Name*</div>
-          <div className="text-wrapper-3">Last Name*</div>
-          <div className="text-wrapper-4">Email*</div>
-          <div className="text-wrapper-5">Phone*</div>
-          <div className="text-wrapper-6">Address</div>
-          <div className="text-wrapper-7">Content requiring advice*</div>
-          <div className="text-wrapper-8">(*)Required information to enter</div>
-          <img className="line" alt="Line" src="line-43.svg" />
-          <img className="img" alt="Line" src="line-47.svg" />
-          <img className="line-2" alt="Line" src="line-44.svg" />
-          <img className="line-3" alt="Line" src="line-45.svg" />
-          <img className="line-4" alt="Line" src="line-48.svg" />
-          <img className="line-5" alt="Line" src="line-46.svg" />
-          <img className="line-6" alt="Line" src="line-49.svg" />
-          <img className="line-7" alt="Line" src="line-50.svg" />
-          <div className="rectangle-2" />
-          <div className="text-wrapper-9">Send</div>
-        </div>
+    <div className="contain">
+      <div className="form-background-contact">
+        <form action="">
+          <div className="form-group-2">
+            <div className="form-group">
+              <label htmlFor="Name">Name*</label>
+              <input
+                type="text"
+                name="FirstName"
+                placeholder="First Name"
+                className="form-control"
+                required
+              />
+              <br />
+              <input
+                type="text"
+                name="LastName"
+                placeholder="Last Name"
+                className="form-control"
+                required
+              />
+            </div>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="email">Email*</label>
+            <input
+              type="email"
+              name="email"
+              className="form-control email-control"
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="message">Message*</label>
+            <textarea
+              name="message"
+              className="form-control message-control"
+              required
+            />
+          </div>
+
+          <div className="form-group-2">
+            <div className="form-group">
+              <label htmlFor="subject">Subject*</label>
+              <input
+                type="text"
+                name="subject"
+                className="form-control subject-control"
+                required
+              />
+            </div>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   );
 };
+
 export default ContactUs;
